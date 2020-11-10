eaa3e163b08adb4e765d227f55e663c1ebeb78a0
bertdk <bertdk10@gmail.com>
2020-11-11 00:44:49 +0100

    fix: styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import s from './app.scss';
+import s from './app.module.scss';
 
 function App() {
   return (
     <div className={s.app}>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <p className={s.text}>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
     </div>
   );
 }
