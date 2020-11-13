5a00cd5a50dc4be34d03fab6627cee51efaea351
bertdk <bertdk10@gmail.com>
2020-11-13 23:37:27 +0100

    feat: get users

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -1,11 +1,14 @@
 import { load, loaded } from 'reducers/user.reducer';
 import { AppThunk } from 'reducers/store';
+import Axios from 'axios';
 import { joinRoom } from 'utils/socket';
 
 export const loadUserAsync = (name: string, room: string): AppThunk => async (dispatch) => {
   dispatch(load());
 
-  await dispatch(loaded({ name, room }));
+  dispatch(loaded({ name, room }));
+
+  await Axios.get('http://localhost:8000/api/users');
 
   joinRoom(name, room);
 };
