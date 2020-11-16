702c8590faaf08aaa49369bb4c784e4a6f6cb3b3
bertdk <bertdk10@gmail.com>
2020-11-16 15:49:27 +0100

    fix: remove axios local

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -1,14 +1,11 @@
 import { load, loaded } from 'reducers/user.reducer';
 import { AppThunk } from 'reducers/store';
-import Axios from 'axios';
 import { joinRoom } from 'utils/socket';
 
 export const loadUserAsync = (name: string, room: string): AppThunk => async (dispatch) => {
   dispatch(load());
 
   dispatch(loaded({ name, room }));
-
-  await Axios.get('http://localhost:8000/api/users');
 
   joinRoom(name, room);
 };
