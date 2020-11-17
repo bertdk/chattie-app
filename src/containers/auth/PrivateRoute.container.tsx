7c5adc8b3db291b2706dd88f303d5b9a00829552
bertdk <bertdk10@gmail.com>
2020-11-17 02:35:41 +0100

    feat: private route

--- src/containers/auth/PrivateRoute.container.tsx
+++ src/containers/auth/PrivateRoute.container.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Redirect, Route } from 'react-router-dom';
-import { selectUserName, selectUserRoom } from 'selectors/user.selector';
+import { selectUserName } from 'selectors/user.selector';
 
-export const PrivateRoute = ({ component, ...rest }: any) => {
-  const username = useSelector(selectUserName);
-  const room = useSelector(selectUserRoom);
-  if (username && room) {
-    return <Route {...rest} render={component} />;
-  }
-  return <Redirect to="/login" />;
+export const PrivateRoute = ({ children, ...rest }: any) => {
+  const user = useSelector(selectUserName);
+  return <Route {...rest} render={() => (user ? children : <Redirect to="/login" />)} />;
 };
